d94e7cd4c48276f80916fe43b16204ecc922e3bd
Daniel Stoyanov <daniel.stoyanov1999@gmail.com>
2025-03-23 14:45:33 +0200

    Add small refactorings in store

--- src/store.rs
+++ src/store.rs
@@ -5,7 +5,8 @@ pub mod store {
 
     static DB_INSTANCE: OnceLock<Mutex<KeyValueStore<String, String>>> = OnceLock::new();
     
-    pub fn get_store() -> &'static Mutex<KeyValueStore<String, String>> {
+    pub fn getStore() -> &'static Mutex<KeyValueStore<String, String>> {
+        // TODO: Handle unsafe unwrapping with unwrap()
         DB_INSTANCE.get_or_init(|| Mutex::new(KeyValueStore::new()))
     }
 
@@ -50,46 +51,46 @@ pub mod store {
 
 #[cfg(test)]
 mod tests {
-    use crate::store::store::{get_store, Store};
+    use crate::store::store::{getStore, Store};
 
     #[test]
     fn test_set() {
-        let store = get_store();
+        let store = getStore();
         let mut store = store.lock().unwrap();
         
-        store.set("name".to_string(), "John".to_string());
-        assert_eq!(store.get(&"name".to_string()), Some("John".to_string()));
+        store.set(String::from("name"), String::from("John"));
+        assert_eq!(store.get(&String::from("name")), Some(String::from("John")));
         
-        store.set("name".to_string(), "Sam".to_string());
-        assert_eq!(store.get(&"name".to_string()), Some("Sam".to_string()));
+        store.set(String::from("name"), String::from("Sam"));
+        assert_eq!(store.get(&String::from("name")), Some(String::from("Sam")));
     }
 
     #[test]
     fn test_get() {
-        let store = get_store();
+        let store = getStore();
         let mut store = store.lock().unwrap();
         
-        store.set("name".to_string(), "John".to_string());
-        store.set("age".to_string(), "20".to_string());
+        store.set(String::from("name"), String::from("John"));
+        store.set(String::from("age"), String::from("20"));
 
-        assert_eq!(store.get(&"name".to_string()), Some("John".to_string()));
-        assert_eq!(store.get(&"age".to_string()), Some("20".to_string()));
+        assert_eq!(store.get(&String::from("name")), Some(String::from("John")));
+        assert_eq!(store.get(&String::from("age")), Some(String::from("20")));
         
-        assert_eq!(store.get(&"nonExisting".to_string()), None);
+        assert_eq!(store.get(&String::from("nonExisting")), None);
     }
 
     #[test]
     fn test_remove() {
-        let store = get_store();
+        let store = getStore();
         let mut store = store.lock().unwrap();
         
-        store.set("name".to_string(), "John".to_string());
-        store.set("age".to_string(), "20".to_string());
+        store.set(String::from("name"), String::from("John"));
+        store.set(String::from("age"), String::from("20"));
 
-        store.remove("name".to_string());
-        assert_eq!(store.get(&"name".to_string()), None);
+        store.remove(String::from("name"));
+        assert_eq!(store.get(&String::from("name")), None);
 
-        store.remove("nonExisting".to_string());
-        assert_eq!(store.get(&"nonExisting".to_string()), None);
+        store.remove(String::from("nonExisting"));
+        assert_eq!(store.get(&String::from("nonExisting")), None);
     }
 }
